3b3b86f48e59a03b51a39a8984bbdd0675c254ee
enzo-rs <enzorojoall@gmail.com>
2021-02-18 15:59:38 +0100

    :hammer: refacto entire code

--- app.js
+++ app.js
@@ -1,175 +1,143 @@
-let containers = document.querySelectorAll('.list-item');
-let prevButton = document.querySelector('.left-button');
-let nextButton = document.querySelector('.right-button');
+const list_item = document.querySelectorAll('.list-item'); 
+const name_box  = document.querySelector('.poke-name');
+const id_box    = document.querySelector('.poke-id');
+const main_screen = document.querySelector('.main-screen');
+const poke_weight = document.querySelector('.poke-weight');
+const poke_height = document.querySelector('.poke-height');
+const poke_type_one  = document.querySelector('.poke-type-one');
+const poke_type_two  = document.querySelector('.poke-type-two');
+const poke_front_img = document.querySelector('.poke-front-image');
+const poke_back_img  = document.querySelector('.poke-back-image');
+const prev_button    = document.querySelector('.left-button');
+const next_button    = document.querySelector('.right-button');
 let prev;
 let next;
 
-function show_pokemon(def_front, def_back) {
-    let pokemon_desc = document.querySelector('.main-section__black');
-    let header_desc = pokemon_desc.querySelector('.screen__header');
-    let main_screen = pokemon_desc.querySelector('.main-screen');
-    let stats = pokemon_desc.querySelector('.screen__stats');
-    let stats_weight = stats.querySelector('.stats__weight');
-    let stats_height = stats.querySelector('.stats__height');
-    let type_one = pokemon_desc.querySelector('.poke-type-one');
-    let type_two = pokemon_desc.querySelector('.poke-type-two');
-
-    let old_type;
-    let new_type;
-    containers.forEach((e) => {
-        e.addEventListener("click", () => {
-            let pointIndex = e.textContent.indexOf('.');
-            let name = e.textContent.slice(pointIndex + 1);
-            let id = e.textContent.slice(0, pointIndex);
-            const finalId = idShape(id);
-            header_desc.querySelector('.poke-name').textContent = name;
-            header_desc.querySelector('.poke-id').textContent = finalId;
-            main_screen.classList.remove('hide');
 
 
-            // Modify background color start
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-                .then(function (response) {
-                    if (response.status == 200) {
-                        response.json()
-                            .then(function (data) {
+async function getPokemon(url) {
 
-                                let pokemon_type = data.types[0].type.name;
-                                old_type = new_type;
-                                new_type = pokemon_type;
-
-
-                                if (new_type !== old_type) {
-                                    main_screen.classList.add(new_type);
-                                    main_screen.classList.remove(old_type);
-                                }
-                                // Modify background color end
-
-                                // Add weight and height start
-                                let weight = data.weight;
-                                let height = data.height;
-                                stats_weight.querySelector('.poke-weight').innerHTML = `${weight}`
-                                stats_height.querySelector('.poke-height').innerHTML = `${height}`
-                                // Add weight and height end
-
-
-                                // Add types start
-                                if (type_two.classList.contains('hide')) {
-                                    type_two.classList.remove('hide')
-                                }
-                                let typea = data.types[0].type.name;
-                                type_one.textContent = typea;
-
-
-                                if (data.types.length === 2) {
-                                    let typeb = data.types[1].type.name;
-                                    type_two.textContent = typeb;
-                                } else {
-                                    type_two.classList.add('hide')
-                                }
-                                // Add types end
-
-
-                                // Add images start
-                                let image_container = pokemon_desc.querySelector('.screen__image');
-                                let back_image = data.sprites[`${def_front}`];
-                                let front_image = data.sprites[`${def_back}`];
-
-                                image_container.querySelector('.poke-front-image').setAttribute('src', front_image);
-                                image_container.querySelector('.poke-back-image').setAttribute('src', back_image);
-                                // Add images end
-
-                            })
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                })
-        });
-    })
-}
-
-
-function idShape(id) {
-    let shape = "#000";
-    let idStr = id.toString()
-    let finalID = shape.slice(0, 4 - idStr.length) + id;
-    return finalID;
-}
-
-class PokemonObject {
-    constructor(product) {
-        this.product = product;
-    }
-
-    createPokemons() {
-        for (let i = 0; i < containers.length; i++) {
-            const element = this.product[i];
-
-            let id;
-            let name;
-            let pokemon_type;
-            fetch(element.url)
-                .then(function (response) {
-                    if (response.status == 200) {
-                        response.json()
-                            .then(function (data) {
-                                id = data.id;
-
-                                name = element.name.charAt(0).toUpperCase() + element.name.slice(1);
-                                let pokeName = `${id}. ${name}`;
-                                containers[i].innerHTML = pokeName;
-                            })
-                    }
-                })
+    if (url == "")
+        throw new Error('url manquant');
+    else {
+        try {
+            const response = await fetch(url);
+            const data     = await response.json();
+            const pokemon_tab = data.results; // Récupérer le tableau de pokemon
+            data_prev     = data.previous;
+            data_next     = data.next;
+          
+            if (!pokemon_tab) {
+                const pokemon_name = data.name
+                const pokemon_id   = data.id;
+                const weight       = data.weight;
+                const height       = data.height;
+                const types        = data.types;
+                const images       = data.sprites;
+                return [pokemon_name, pokemon_id, types, weight, height, images];
+            }
+            return [pokemon_tab, data, data_prev, data_next];
+        } catch (err) {
+            throw new Error(err)
         }
     }
 }
 
-// Next and Prev Buttons end
 
-function printPokemons(url) {
-    fetch(url)
-        .then(function (response) {
-            if (response.status == 200) {
-                response.json()
-                    .then(function (data) {
-                        let pokemon_tab = data.results;
-                        let pokemonObj = new PokemonObject(pokemon_tab);
-                        pokemonObj.createPokemons()
-                        prev = data.previous;
-                        next = data.next;
-                        let old_prev;
-                        let old_next;
+function toUpperCase(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+function idShape(id) {
+    let shape = "#000";
+    let idStr = id.toString()
+    return shape.slice(0, 4 - idStr.length) + id;
+}
 
 
-                        show_pokemon("front_default", "back_default");
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    })
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+async function show_poke_info(data_info, item) {
+    item.addEventListener('click', () => {
+        const pokemon_name   = data_info[0];
+        const pokemon_id     = data_info[1];
+        const pokemon_types  = data_info[2];
+        const pokemon_weight = data_info[3];
+        const pokemon_height = data_info[4];
+        const pokemon_images = data_info[5];    
+
+
+        name_box.innerHTML = toUpperCase(pokemon_name);
+        id_box.innerHTML   = idShape(pokemon_id);
+        
+
+        // Type info start
+        let type_one = pokemon_types[0].type.name;
+        poke_type_one.innerHTML = type_one
+        if (pokemon_types.length == 2) {
+            poke_type_two.classList.remove('hide');
+            let type_two = pokemon_types[1].type.name;
+            poke_type_two.innerHTML = type_two;
+        } else {
+            poke_type_two.classList.add('hide')
+        }
+        // Type info end
+
+        // change background color start
+        main_screen.className = '';
+        main_screen.classList.add('main-screen', type_one)
+        // change background color end
+
+        poke_weight.innerHTML = pokemon_weight;
+        poke_height.innerHTML = pokemon_height;
+
+
+        // Pokemon images start
+        const back_image  = pokemon_images["back_default"];
+        const front_image = pokemon_images["front_default"];
+
+        poke_front_img.setAttribute('src', front_image);
+        poke_back_img.setAttribute('src', back_image);                                      
+        // Pokemon images end
+
+
+    })
 }
 
 
 
 
-prevButton.addEventListener('click', () => {
-    if (prev !== null) {
-        printPokemons(prev);
+async function show_pokemons(url = 'https://pokeapi.co/api/v2/pokemon/') {
+    const poke_data = await getPokemon(url);
+    
+    const pokemon_tab = poke_data[0];
+    const data = poke_data[1];
+    prev = poke_data[2];
+    next = poke_data[3];
+    for (let i = 0; i < pokemon_tab.length; i++) {
+        const element = pokemon_tab[i];
+        
+        const poke_data_info = await getPokemon(element.url);
+        let poke_name = `${poke_data_info[1]}. ${toUpperCase(element.name)}`;
+        list_item[i].innerHTML = poke_name; // On affiche le nom et l'id de nos pokemons
+
+        show_poke_info(poke_data_info, list_item[i]);
+
     }
-});
+};
 
 
-nextButton.addEventListener('click', () => {
-    if (next !== null) {
-        printPokemons(next);
+function previousPage() {
+    if (prev) {
+        show_pokemons(prev);
     }
-});
+}
+function nextPage() {
+    if (next) {
+        show_pokemons(next);
+    }
+}
 
+prev_button.addEventListener('click', previousPage);
+next_button.addEventListener('click', nextPage);
 
-printPokemons('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
+show_pokemons();
+
--- script.js
+++ script.js
@@ -1,197 +1,45 @@
-let box = document.querySelector('.controllers__d-pad')
-let bottom_button = box.querySelector('div.bottom');
-let left_button = box.querySelector('div.left');
-let right_button = box.querySelector('div.right');
-let top_button = box.querySelector('div.top');
-containers = document.querySelectorAll('.list-item');
-let buttonA = document.querySelector('.controllers__buttons .buttons__button:last-child')
 let code = "rAtlBlAb";
-let second_code = "bAlBltAr";
+
 
 // KONAMI code start
 
-
 let str = [];
-let elements = box.querySelectorAll('.d-pad__cell');
-let buttons_el = document.querySelectorAll('.buttons__button');
-
+let elements = document.querySelectorAll('div.btn');
 
 function changeColor() {
     let right_container = document.querySelector('.right-container');
     let left_container = document.querySelector('.left-container');
     let left_el = document.querySelectorAll('.left-container__hinge');
 
-
     right_container.classList.add('gold');
     left_container.classList.add('gold');
     left_el.forEach((e) => {
         e.classList.add('gold_gradient');
     })
-
 }
 
-
-function toShiny(key) {
+function konamiCode(key) {
     str.push(key);
     console.log(str);
     if (str.join('') === code) {
         changeColor()
-    } else if (str.join('') === second_code) {
-        show_pokemon("front_shiny", "back_shiny")
-    } else {
-        show_pokemon("front_default", "back_default")
-    }
+    } 
 
     if (str.length >= code.length) {
         str.splice(0, 1)
-    }
+    } 
 }
 
 elements.forEach((e) => {
     e.addEventListener('click', () => {
         let key = e.getAttribute('data-key');
-        toShiny(key);
+        konamiCode(key);
     })
 })
 
-buttons_el.forEach((e) => {
-    e.addEventListener('click', () => {
-        let key = e.textContent;
-        toShiny(key);
-    })
-})
 
 // KONAMI code end
 
-
-let count = 0;
-
-bottom_button.addEventListener('click', () => {
-    count++;
-    
-    selectItem()
-})
-
-top_button.addEventListener('click', () => {
-    count--;
-    
-    selectItem()
-})
-
-right_button.addEventListener('click', () => {
-    count += 10;
-    
-    selectItem()
-})
-
-left_button.addEventListener('click', () => {
-    count -= 10;
-    
-    selectItem()
-})
-
-function selectItem() {
-    for (let i = 0; i < containers.length; i++) {
-        const element = containers[i];
-
-        if (count === i) {
-            element.classList.add('selection')
-        } else if (element.classList.contains('selection')) {
-            element.classList.remove('selection')
-        }
-    }
-}
-
-
-
-
-
-buttonA.addEventListener('click', () => {
-    let pokemon_desc = document.querySelector('.main-section__black');
-    let header_desc = pokemon_desc.querySelector('.screen__header');
-    let main_screen = pokemon_desc.querySelector('.main-screen');
-    let stats = pokemon_desc.querySelector('.screen__stats');
-    let stats_weight = stats.querySelector('.stats__weight');
-    let stats_height = stats.querySelector('.stats__height');
-    let type_one = pokemon_desc.querySelector('.poke-type-one');
-    let type_two = pokemon_desc.querySelector('.poke-type-two');
-
-    let old_type;
-    let new_type;
-    containers.forEach((e) => {
-        if (e.classList.contains('selection')) {
-
-
-
-
-            let pointIndex = e.textContent.indexOf('.');
-            let name = e.textContent.slice(pointIndex + 1);
-            let id = e.textContent.slice(0, pointIndex);
-            const finalId = idShape(id);
-            header_desc.querySelector('.poke-name').textContent = name;
-            header_desc.querySelector('.poke-id').textContent = finalId;
-            main_screen.classList.remove('hide');
-
-
-            // Modify background color start
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-                .then(function (response) {
-                    if (response.status == 200) {
-                        response.json()
-                            .then(function (data) {
-
-                                let pokemon_type = data.types[0].type.name;
-                                main_screen.className = '';
-                                main_screen.classList.add("main-screen", pokemon_type)
-
-
-                                // Modify background color end
-
-                                // Add weight and height start
-                                let weight = data.weight;
-                                let height = data.height;
-                                stats_weight.querySelector('.poke-weight').innerHTML = `${weight}`
-                                stats_height.querySelector('.poke-height').innerHTML = `${height}`
-                                // Add weight and height end
-
-
-                                // Add types start
-                                if (type_two.classList.contains('hide')) {
-                                    type_two.classList.remove('hide')
-                                }
-                                let typea = data.types[0].type.name;
-                                type_one.textContent = typea;
-
-
-                                if (data.types.length === 2) {
-                                    let typeb = data.types[1].type.name;
-                                    type_two.textContent = typeb;
-                                } else {
-                                    type_two.classList.add('hide')
-                                }
-                                // Add types end
-
-
-                                // Add images start
-                                let image_container = pokemon_desc.querySelector('.screen__image');
-                                let back_image = data.sprites[`back_default`];
-                                let front_image = data.sprites[`front_default`];
-
-                                image_container.querySelector('.poke-front-image').setAttribute('src', front_image);
-                                image_container.querySelector('.poke-back-image').setAttribute('src', back_image);
-                                // Add images end
-
-                            })
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                })
-        }
-    })
-
-
-})
 
 
 
